93a1a315ff735eeb38755b6b508e83ffad032196
Joshen96 <asdzxc250@naver.com>
2023-11-02 17:35:23 +0900

    진행중

--- Assets/Strategy_Pattern/Scripts/BulletMove.cs
+++ Assets/Strategy_Pattern/Scripts/BulletMove.cs
@@ -7,12 +7,15 @@ public class BulletMove : MonoBehaviour
     [SerializeField]
     float speed =10f;
 
+    
+
     private void Update()
     {
         this.transform.Translate(Vector3.up * speed * Time.deltaTime);
 
-        if(this.transform.position.y>10.0f)
+        if(this.transform.position.y>10f)
         {
+
             Destroy(this.gameObject);
         }
     }
